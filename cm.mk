81d64defff0997237842ff519ad2f32b7849c179
Jacob Rigby <jacobcrigby@gmail.com>
2012-05-29 12:17:20 -0500

    Remove version number from cm.mk
    We don't need to actually keep track of the release number in the code.
    We can just rename files and keep track of it manualy ourselves. Also,
    Git tags for releases. This should be more CM-like, as well as easier to
    maintain.
    
    Change-Id: I220c4dbbc0f29c8d1aff0c6bb36d2649675e3381

--- cm.mk
+++ cm.mk
@@ -18,9 +18,8 @@ PRODUCT_RELEASE_NAME := HTC Droid Incredible
 #Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRJ22 PRODUCT_NAME=inc BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys PRODUCT_BRAND=verizon_wwe TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="4.08.605.2 CL185897 release-keys" PRODUCT_MODEL=ADR6300 PRODUCT_MANUFACTURER=HTC
 
-# Set up the product codename & build version
+# Set up the product codename
 PRODUCT_CODENAME := CM9
-PRODUCT_VERSION_DEVICE_SPECIFIC := a9
 
 PRODUCT_PACKAGES += \
     Camera \
