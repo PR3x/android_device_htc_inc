e433b9e45f4194513b6768a11020917e2bd9e699
Jacob Rigby <jacobcrigby@gmail.com>
2012-06-04 16:00:58 -0500

    Clean up cm.mk
    Removed unnecessary define
    Tell cm this is a cdma phone
    Fix whitespace issue
    
    Change-Id: Ic732ed0b795d02d780d516a42cfd2976010c6177

--- cm.mk
+++ cm.mk
@@ -1,3 +1,6 @@
+# Specify phone tech here before calling full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := INCREDIBLE
 
@@ -18,9 +21,6 @@ PRODUCT_RELEASE_NAME := HTC Droid Incredible
 #Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRJ22 PRODUCT_NAME=inc BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys PRODUCT_BRAND=verizon_wwe TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="4.08.605.2 CL185897 release-keys" PRODUCT_MODEL=ADR6300 PRODUCT_MANUFACTURER=HTC
 
-# Set up the product codename
-PRODUCT_CODENAME := CM9
-
 PRODUCT_PACKAGES += \
     Camera \
-	Torch
+    Torch
